a63c25e6637c0320d18eff8671b31d173a2bd16f
Joel Nothman <joel.nothman@gmail.com>
2018-02-23 01:46:23 +1100

    Better fix for matplotlib 1

--- upsetplot/plotting.py
+++ upsetplot/plotting.py
@@ -254,10 +254,10 @@ class UpSet:
 
         tick_axis = ax.yaxis
         tick_axis.set_ticks(np.arange(n_sets))
-        tick_axis.set_ticklabels(data.index.names)
+        tick_axis.set_ticklabels(data.index.names,
+                                 rotation=0 if self._horizontal else -90)
         ax.xaxis.set_visible(False)
-        ax.tick_params(axis='both', which='both', length=0,
-                       labelrotation=0 if self._horizontal else -90)
+        ax.tick_params(axis='both', which='both', length=0)
         if not self._horizontal:
             ax.yaxis.set_ticks_position('top')
         ax.set_frame_on(False)
