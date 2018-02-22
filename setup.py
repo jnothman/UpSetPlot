dbf0fa2ff509c2c9963adb429745d6d6c74136d8
Joel Nothman <joel.nothman@gmail.com>
2018-02-22 13:25:51 +1100

    Fix/update metadata

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ import os
 import sys
 from setuptools import setup
 
+from upsetplot import __version__ as version
+
 
 def setup_package():
     src_path = os.path.dirname(os.path.abspath(sys.argv[0]))
@@ -14,8 +16,19 @@ def setup_package():
     try:
         # See setup.cfg
         setup(name='UpSetPlot',
-              version='0.1a',
-              py_modules=['upsetplot'],
+              version=version,
+              packages=["upsetplot"],
+              classifiers=[
+                  'Development Status :: 3 - Alpha',
+                  'Intended Audience :: Science/Research',
+                  'Topic :: Scientific/Engineering :: Visualization',
+                  'Programming Language :: Python :: 3'
+                  'Programming Language :: Python :: 3.4'
+                  'Programming Language :: Python :: 3.5'
+                  'Programming Language :: Python :: 3.6'
+                  'Programming Language :: Python :: 2.7'
+              ],
+              license="BSD",
               setup_requires=['pytest-runner'],
               tests_require=['pytest>=2.7', 'pytest-cov~=2.4'],
               # TODO: check versions
