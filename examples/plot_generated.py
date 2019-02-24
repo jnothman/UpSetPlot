a74a7f3546e1706f41be5710c87ff263981ae60e
Joel Nothman <joel.nothman@gmail.com>
2019-02-24 20:20:44 +1100

    Add show_sizes option
    Fixes #5

--- examples/plot_generated.py
+++ examples/plot_generated.py
@@ -13,7 +13,13 @@ example = generate_data(aggregated=True)
 print(example)
 
 plot(example)
+plt.title('Ordered by degree')
 plt.show()
 
 plot(example, sort_by='cardinality')
+plt.title('Ordered by cardinality')
+plt.show()
+
+plot(example, size_fmt='%d')
+plt.title('With size shown')
 plt.show()
--- examples/plot_vertical.py
+++ examples/plot_vertical.py
@@ -11,4 +11,9 @@ from upsetplot import generate_data, plot
 
 example = generate_data(aggregated=True)
 plot(example, orientation='vertical')
+plt.title('A vertical plot')
+plt.show()
+
+plot(example, orientation='vertical', size_fmt='%d')
+plt.title('A vertical plot with sizes')
 plt.show()
