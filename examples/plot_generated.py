689b6fa0905eaac70ba4b25e058a7338dffd5def
Joel Nothman <joel.nothman@gmail.com>
2019-02-25 00:31:31 +1100

    EXA fix title -> suptitle

--- examples/plot_generated.py
+++ examples/plot_generated.py
@@ -13,13 +13,13 @@ example = generate_data(aggregated=True)
 print(example)
 
 plot(example)
-plt.title('Ordered by degree')
+plt.suptitle('Ordered by degree')
 plt.show()
 
 plot(example, sort_by='cardinality')
-plt.title('Ordered by cardinality')
+plt.suptitle('Ordered by cardinality')
 plt.show()
 
 plot(example, show_counts='%d')
-plt.title('With counts shown')
+plt.suptitle('With counts shown')
 plt.show()
