825c8fa74283b18af11eed70938459fbfb248fcc
Joel Nothman <joel.nothman@gmail.com>
2019-03-07 19:50:03 +0800

    Increase version

--- upsetplot/__init__.py
+++ upsetplot/__init__.py
@@ -1,6 +1,6 @@
 from .plotting import UpSet, plot
 from .data import generate_data, from_memberships
 
-__version__ = '0.2-dev'
+__version__ = '0.3-dev'
 
 __all__ = ['UpSet', 'generate_data', 'plot', 'from_memberships']
