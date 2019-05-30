f56bb69ab02e6b5827db8ef90aca36ce5af4c4af
Joel Nothman <joel.nothman@gmail.com>
2019-05-30 22:35:18 +1000

    Add to __all__

--- upsetplot/__init__.py
+++ upsetplot/__init__.py
@@ -3,4 +3,5 @@ from .data import generate_data, from_memberships, from_contents
 
 __version__ = '0.3-dev'
 
-__all__ = ['UpSet', 'generate_data', 'plot', 'from_memberships']
+__all__ = ['UpSet', 'generate_data', 'plot',
+           'from_memberships', 'from_contents']
