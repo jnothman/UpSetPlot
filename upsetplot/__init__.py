c57f030debd13076150b754e4236ccbf9743caae
Joel Nothman <joel.nothman@gmail.com>
2019-06-06 18:45:41 +1000

    Update __init__.py

--- upsetplot/__init__.py
+++ upsetplot/__init__.py
@@ -1,5 +1,5 @@
 from .plotting import UpSet, plot
-from .data import (generate_counts, generate_data, generate_samples
+from .data import (generate_counts, generate_data, generate_samples,
                    from_memberships, from_contents)
 
 __version__ = '0.3-dev'
