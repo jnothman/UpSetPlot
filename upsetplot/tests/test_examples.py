2fbffe6211d3aa9d0e7676d0290b70a09891e2a0
Joel Nothman <joel.nothman@gmail.com>
2019-06-06 13:14:57 +1000

    Deps for examples

--- upsetplot/tests/test_examples.py
+++ upsetplot/tests/test_examples.py
@@ -8,6 +8,9 @@ import pytest
 exa_glob = os.path.join(os.path.dirname(os.path.abspath(__file__)),
                         '..', '..', 'examples', '*.py')
 
+
 @pytest.mark.parametrize('path', glob.glob(exa_glob))
 def test_example(path):
+    pytest.importorskip('sklearn')
+    pytest.importorskip('seaborn')
     subprocess.check_output(['python', path])
