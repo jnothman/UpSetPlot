0e4a0b2be6153e32725b62fdab10bbabe0046cd4
Joel Nothman <joel.nothman@gmail.com>
2019-07-04 16:35:56 +1000

    MNT Remove debug output. Embarrassing!

--- upsetplot/__init__.py
+++ upsetplot/__init__.py
@@ -3,8 +3,6 @@ __version__ = '0.4-dev'
 import os
 
 if os.environ.get('__in-setup', None) != '1':
-    import sys
-    print(os.environ, file=sys.stderr)
     from .plotting import UpSet, plot
     from .data import (generate_counts, generate_data, generate_samples,
                        from_memberships, from_contents)
