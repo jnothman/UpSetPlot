804b8014101d8c4cd40d7e9c563750001fcd003b
Joel Nothman <joel.nothman@gmail.com>
2019-07-04 16:29:16 +1000

    MNT fix installation without dependencies

--- upsetplot/__init__.py
+++ upsetplot/__init__.py
@@ -1,10 +1,15 @@
-from .plotting import UpSet, plot
-from .data import (generate_counts, generate_data, generate_samples,
-                   from_memberships, from_contents)
-
 __version__ = '0.3.0.post1'
 
-__all__ = ['UpSet',
-           'generate_data', 'generate_counts', 'generate_samples',
-           'plot',
-           'from_memberships', 'from_contents']
+import os
+
+if os.environ.get('__in-setup', None) != '1':
+    import sys
+    print(os.environ, file=sys.stderr)
+    from .plotting import UpSet, plot
+    from .data import (generate_counts, generate_data, generate_samples,
+                       from_memberships, from_contents)
+
+    __all__ = ['UpSet',
+               'generate_data', 'generate_counts', 'generate_samples',
+               'plot',
+               'from_memberships', 'from_contents']
