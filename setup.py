6f5d4e6546c715dac3152dda487db15eab1b18a4
Joel Nothman <joel.nothman@gmail.com>
2019-07-04 16:29:16 +1000

    MNT fix installation without dependencies

--- setup.py
+++ setup.py
@@ -4,8 +4,6 @@ import os
 import sys
 from setuptools import setup
 
-from upsetplot import __version__ as version
-
 
 def setup_package():
     src_path = os.path.dirname(os.path.abspath(sys.argv[0]))
@@ -14,14 +12,16 @@ def setup_package():
     sys.path.insert(0, src_path)
 
     try:
-        # See setup.cfg
+        os.environ['__in-setup'] = '1'  # ensures only version is imported
+        from upsetplot import __version__ as version
+        # See also setup.cfg
         setup(name='UpSetPlot',
               version=version,
               packages=["upsetplot"],
               setup_requires=['pytest-runner'],
               tests_require=['pytest>=2.7', 'pytest-cov<2.6'],
               # TODO: check versions
-              install_requires=['numpy', 'pandas', 'matplotlib>=2.0'])
+              install_requires=['pandas', 'matplotlib>=2.0'])
     finally:
         del sys.path[0]
         os.chdir(old_path)
