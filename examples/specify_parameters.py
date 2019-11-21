30815755f33b51b9baca1722f12a5771aa139054
JH Liu <liujh26@mail2.sysu.edu.cn>
2019-11-21 13:41:15 +0800

    Add files via upload

--- examples/specify_parameters.py
+++ examples/specify_parameters.py
@@ -12,62 +12,69 @@ from upsetplot import generate_counts, plot
 example = generate_counts()
 print(example)
 
-
-# provide kws matrix
+# specify barplot colors and size
 plot(example, sort_by='cardinality',
-     color_by_row={"cat1": "orange", "cat2": "green"},
-     matrix_kws={"s": 1}, lines_kws={"colors": "green", "linestyles": "--"})
-plt.suptitle('Ordered by cardinality')
-plt.show()
-
-# provide kws bars
-plot(example, sort_by='cardinality',
-     color_by_row={"cat1": "orange", "cat2": "green"},
-     intersection_kws={"width": 1.0},
-     totals_kws={"color": "purple", "width": 0.2})
-plt.suptitle('Ordered by cardinality')
-plt.show()
-
-# to test if orientation change works
-plot(example, sort_by='cardinality',
-     color_by_row={"cat1": "orange", "cat2": "green"},
-     intersection_kws={"width": 1.0},
-     totals_kws={"color": "purple", "width": 0.2},
-     orientation="verticle")
-plt.suptitle('Ordered by cardinality')
+     intersection_facecolor="purple",
+     intersection_width=1.0,
+     totals_facecolor="brown",
+     totals_width=0.2)
+plt.suptitle('Barplot colors and width')
 plt.show()
 
 # color by row
 plot(example, sort_by='cardinality',
      color_by_row={"cat1": "orange", "cat2": "green"})
-plt.suptitle('Ordered by cardinality')
+plt.suptitle('Colored by row')
 plt.show()
 
 # color by row, and color the barplot
 plot(example, sort_by='cardinality',
      color_by_row={"cat1": "orange", "cat2": "green"},
      totals_as_row=True)
-plt.suptitle('Ordered by cardinality')
+plt.suptitle('Colored by row (and barplot)')
 plt.show()
 
 # color by col
-plot(example, sort_by='cardinality', color_by_col={("cat0", "cat1"): "orange"})
+plot(example, sort_by='cardinality',
+     color_by_col={("cat0", "cat1"): "orange"})
 plt.suptitle('Ordered by cardinality')
+plt.suptitle('Colored by column')
 plt.show()
 
 # color by coland color the barplot
-plot(example, sort_by='cardinality', color_by_col={("cat0", "cat1"): "orange"},
+plot(example, sort_by='cardinality',
+     color_by_col={("cat0", "cat1"): "orange"},
      intersection_as_col=True)
 plt.suptitle('Ordered by cardinality')
+plt.suptitle('Colored by column (and barplot)')
 plt.show()
 
-# modify lines
-plot(example, sort_by='cardinality', lines_color="darkgray", lines_width=10)
-plt.suptitle('Ordered by cardinality')
-plt.show()
-
-# grids
+# modify matrix lines
 plot(example, sort_by='cardinality',
-     intersection_grids=False, totals_grids=False)
+     matrix_line_color="darkgray",
+     matrix_line_width=10)
 plt.suptitle('Ordered by cardinality')
+plt.suptitle('Matrix lines')
+plt.show()
+
+# modify dots color
+plot(example, sort_by='cardinality',
+     empty_dot_color="lightgreen",
+     dot_color="darkgreen")
+plt.suptitle('Ordered by cardinality')
+plt.suptitle('Empty dot colors')
+plt.show()
+
+# to test if orientation change works
+plot(example, sort_by='cardinality',
+     intersection_facecolor="purple",
+     intersection_width=1.0,
+     totals_facecolor="brown",
+     totals_width=0.2,
+     empty_dot_color="lightgreen",
+     matrix_line_color="darkgray",
+     matrix_line_width=10,
+     dot_color="darkgreen",
+     orientation="verticle")
+plt.suptitle('verticle')
 plt.show()
