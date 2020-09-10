7723f374a557202744a4e6f107da64dd5443c0b0
Joel Nothman <joel.nothman@gmail.com>
2020-09-10 15:51:04 +1000

    Don't use global python in tests

--- upsetplot/tests/test_examples.py
+++ upsetplot/tests/test_examples.py
@@ -1,6 +1,7 @@
 import glob
 import os
 import subprocess
+import sys
 
 import pytest
 
@@ -13,4 +14,4 @@ exa_glob = os.path.join(os.path.dirname(os.path.abspath(__file__)),
 def test_example(path):
     pytest.importorskip('sklearn')
     pytest.importorskip('seaborn')
-    subprocess.check_output(['python', path])
+    subprocess.check_output([sys.executable, path])
