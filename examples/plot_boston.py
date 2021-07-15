05ad886c63dbe2090f824d9f1787dce37c934f35
Joel Nothman <joel.nothman@gmail.com>
2021-07-15 11:32:40 +1000

    Split examples into cells to display code + plot

--- examples/plot_boston.py
+++ examples/plot_boston.py
@@ -53,6 +53,8 @@ boston_df = boston_df.set_index(list(boston_above_avg.columns))
 # Also give us access to the target (median house value)
 boston_df = boston_df.assign(median_value=boston.target)
 
+##########################################################################
+
 # UpSet plot it!
 upset = UpSet(boston_df, subset_size='count', intersection_plot_elements=3)
 upset.add_catplot(value='median_value', kind='strip', color='blue')
@@ -60,6 +62,8 @@ upset.add_catplot(value='AGE', kind='strip', color='black')
 upset.plot()
 plt.title("UpSet with catplots, for orientation='horizontal'")
 plt.show()
+
+##########################################################################
 
 # And again in vertical orientation
 
