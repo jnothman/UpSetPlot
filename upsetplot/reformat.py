a79b8bd7108e8936191f2ebbb6eeda17c0e19faf
Simon Li <orpheus+devel@gmail.com>
2022-12-04 12:42:17 +0000

    Fix RTD formatting for docstring examples (#188)

--- upsetplot/reformat.py
+++ upsetplot/reformat.py
@@ -255,7 +255,8 @@ def query(data, present=None, absent=None,
               False      2  2.05...
               False     10  2.55...
 
-    # sorting
+    Sorting
+
     >>> query(data, min_degree=1, sort_by="degree").subset_sizes
     cat1   cat2   cat0
     True   False  False    11
