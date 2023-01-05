22c39b44e45db17fb41f3dfd1deebfde542d403e
Joel Nothman <joel.nothman@gmail.com>
2023-01-05 22:51:41 +1100

    make the limit 62 to be sure

--- upsetplot/plotting.py
+++ upsetplot/plotting.py
@@ -43,7 +43,7 @@ def _process_data(df, sort_by, sort_categories_by, subset_size,
     def _pack_binary(X):
         X = pd.DataFrame(X)
         # we use objects when there are many categories for arbitrary precision integers
-        dtype = np.object if X.shape[1] > 63 else np.uint64
+        dtype = np.object if X.shape[1] > 62 else np.uint64
         out = pd.Series(0, index=X.index, dtype=dtype)
         for i, (_, col) in enumerate(X.items()):
             out *= 2
